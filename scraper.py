2c4118c14af52f486ca214687c385d5cba502473
Dave Man <dmn001@gmail.com>
2016-02-24 16:47:00 +0000

    remove functionality to get all games

--- scraper.py
+++ scraper.py
@@ -6,8 +6,6 @@ import unicodecsv
 
 class scraper():
     def __init__(self):
-        self.game_name_for_appid = {}
-        self.init_unicodecsv()
         self.day_range = '9223372036854776000'
         self.language = 'english'
 
@@ -40,7 +38,7 @@ class scraper():
                     has_new_data = True
                     self.recommendation_ids.append(recommendation_id)
                 persona_name = review.find('div',class_="persona_name").get_text(strip=True).replace("\n",'|')
-                row = [app_id, self.game_name_for_appid.get(app_id), json['recommendationids'][index], type, persona_name, review_text ]
+                row = [app_id, json['recommendationids'][index], type, persona_name, review_text ]
                 self.csv_unicode_writer.writerow(row)
             print len(self.recommendation_ids)
             return has_new_data
@@ -51,41 +49,17 @@ class scraper():
         self.csv_fh = codecs.open(filename, 'wb')
         self.csv_fh.write(u'\uFEFF'.encode('utf8'))
         self.csv_unicode_writer = unicodecsv.writer(self.csv_fh, encoding='utf-8')
-        header = ['appid','game_name','id','type','username','text']
+        header = ['appid','id','type','username','text']
         self.csv_unicode_writer.writerow(header)
-
-    def get_reviews_for_top_100_games(self,type=None,pages=1):
-        self.get_top_games_by_player_count()
-        if type is None:
-            type = 'all'
-        count = 1
-        for appid in sorted(self.game_name_for_appid,key=int):
-            for page in range(pages):
-                print count, page, appid, self.game_name_for_appid[appid]
-                offset = page * 20
-                self.get_reviews_for_appid(appid, offset=offset, type=type)
-            count = count + 1
-
-    def get_top_games_by_player_count(self):
-        url = 'http://store.steampowered.com/stats/'
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "lxml")
-        links = soup.find_all('a',class_="gameLink")
-        for item in links:
-            url = item['href']
-            game_name = item.get_text().encode('utf-8')
-            m = re.search('app/(\d+)/',url)
-            if m:
-                self.game_name_for_appid[m.group(1)] = game_name
 
     def get_all_reviews_for_appid(self,app_id,type):
         self.get_reviews_for_appid(app_id, type, 0)
         self.get_reviews_for_appid(app_id, type, 5)
         offset = 25
         while self.get_reviews_for_appid(app_id, type, offset):
-            offset += 20
+            offset += 25
 
 if __name__ == "__main__":
     s = scraper()
-
+    s.init_unicodecsv()
     s.get_all_reviews_for_appid('427820', type='all')

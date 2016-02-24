4dc19ceb04751dec1f00f9c48c6abc91ea828856
Dave Man <dmn001@gmail.com>
2016-02-24 16:19:52 +0000

    rename methods and args

--- scraper.py
+++ scraper.py
@@ -11,19 +11,7 @@ class scraper():
         self.day_range = 180
         self.language = 'english'
 
-    def get_top_games_by_player_count(self):
-        url = 'http://store.steampowered.com/stats/'
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "lxml")
-        links = soup.find_all('a',class_="gameLink")
-        for item in links:
-            url = item['href']
-            game_name = item.get_text().encode('utf-8')
-            m = re.search('app/(\d+)/',url)
-            if m:
-                self.game_name_for_appid[m.group(1)] = game_name
-
-    def get_reviews_for_appid(self,app_id=None,offset=0,type=None):
+    def get_reviews_for_appid(self,app_id=None,type=None,offset=0):
         if type is None:
             type = 'all'
 
@@ -56,7 +44,7 @@ class scraper():
         header = ['appid','game_name','id','type','username','text']
         self.csv_unicode_writer.writerow(header)
 
-    def get_reviews_for_all_games(self,type=None,pages=1):
+    def get_reviews_for_top_100_games(self,type=None,pages=1):
         self.get_top_games_by_player_count()
         if type is None:
             type = 'all'
@@ -68,9 +56,19 @@ class scraper():
                 self.get_reviews_for_appid(appid, offset=offset, type=type)
             count = count + 1
 
+    def get_top_games_by_player_count(self):
+        url = 'http://store.steampowered.com/stats/'
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "lxml")
+        links = soup.find_all('a',class_="gameLink")
+        for item in links:
+            url = item['href']
+            game_name = item.get_text().encode('utf-8')
+            m = re.search('app/(\d+)/',url)
+            if m:
+                self.game_name_for_appid[m.group(1)] = game_name
+
 if __name__ == "__main__":
     s = scraper()
-    # s.get_top_games_by_player_count()
 
-    # s.get_reviews_for_appid('730', offset=0, 'funny')
-    s.get_reviews_for_appid('427820', offset=0, type='all')
+    s.get_reviews_for_appid('427820', type='all')
